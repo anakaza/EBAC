c6919d12815c4ac4d181a01e642f6f06d23fdad7
azk <linarelolcs@gmail.com>
2024-02-16 09:56:27 -0300

    Initial commit

---
+++ tarefa.js
@@ -0,0 +1,11 @@
+/*Calcula area simples*/
+
+let base = prompt('Digite o valor da base');
+let altura = prompt('Digite o valor da altura');
+
+function calculaArea() {
+  return base * altura;
+
+}
+
+calculaArea();
