23f4de28e9ca3dd913c0252abbd8aa138399da1d
azk <linarelolcs@gmail.com>
2024-02-16 10:31:53 -0300

    Initial commit

--- main.js
+++ main.js
@@ -1,46 +1,71 @@
-const form = document.getElementById('form-contatos'); /*Resgata formulário*/
-const contato=[]; /*Array*/
-const numero=[]; /*Array*/
+/* Antes de qualquer coisa, sigo com tudo que já nos foi ensinado até o momento, com outra lógica.*/
 
-let linhas ='';
+$(document).ready(function(){
+        var formVisivel = false;
 
-form.addEventListener('submit', function(e){
-    e.preventDefault(); /*Previne o comportamento padrão do botão*/
-    adicionaLinha(); /*Chama função*/
-    atualizaTabela(); /*chama função*/
+    $('#btn-fixo').click(function() {
+        if(!formVisivel){
+            $('form').slideDown(); 
+            formVisivel = true;
+            $(this).addClass('active');         /* Troquei o  "$('button header').add..." por this facilitando a aplicação o código.*/
+            $('#add-img').hide();
+            $('#rmv-img').show();
+
+            /*Ao ativar o botão "+" traz a página para o topo do formulário melhorando a interação do usuário*/
+            /*Código de pesquisa. Serve para rolar a página para acompanhar as ações do botão */
+            $('html, body').animate({ 
+                scrollTop: $('form').offset().top
+            }, 'slow');
+
+        } else{
+            $('form').slideUp();
+            formVisivel = false;
+            $(this).removeClass('active');       /* Troquei o  "$('button header').add..." por this facilitando a aplicação do código.*/
+            $('#add-img').show();
+            $('#rmv-img').hide(); 
+
+            //fica a seus critério permitir arrastar a página após fechar o formulário
+
+            /* 
+            $('html, body').animate({ 
+                scrollTop: $('form').offset().top
+            }, 'slow');
+            */
+
+        }
+});
+
+    
+    $('form').on('submit', function(e){
+        e.preventDefault();
+
+        const enderecoDaNovaImagem = $('#endereco-image-nova').val(); //Add url elemento dinamico
+        
+        /*Criação de um novo item para a lista*/
+        const novoItem =$('<li></li>');
+
+        /*Adiciona a imagem resgatada (const enderecoDaNovaImagem) e define o elemento de destino (const novoItem)*/
+        $(`<img src="${enderecoDaNovaImagem}"/>`).appendTo(novoItem); 
+        
+        /*Adiciona o overlay com o link para a imagem adicionada*/
+        $(`
+            <div class="overlay-image-link">
+                <a href = "${enderecoDaNovaImagem}" target="_blank" title="Abrir">
+                    Abrir
+                </a>
+            </div>
+        `).appendTo(novoItem);
+
+        
+        $(novoItem).appendTo('ul'); /*Adicionando o novo item na ul do html*/
+        $('#endereco-image-nova').val(""); /*limpa o campo da url*/
+
+        /*rola para baixo, após a adição, para que o usuário acompanhe a adição de imagens*/
+        $('html, body').animate({ 
+            scrollTop: $('form').offset().top
+        }, 'slow');
+
+    })
+
+
 })
-
-/*Função para criar linhas e inserir os dados do formulário.*/
-function adicionaLinha(){
-    const inputnomeContato = document.getElementById('nome-contato'); /*Variavel(constante) que recebe o valor do nome do contato*/
-    const inputnumeroContato = document.getElementById('numero-contato'); /* variavel (constante) que recebe o valor do número do contato*/
-
-    /* Condição "IF" para validar se o número ou nome já foram adicionados anteriormente.*/
-    /* || = "ou" */
-
-    if (contato[inputnomeContato.value] || numero.includes(parseFloat(inputnumeroContato.value))) {
-        alert(`Este contato ou número já foi salvo anteriormente.`);
-        /*O parse float converte o número de string para número de ponto flutuante, sei que não é o ideal, mas foi a única maneira, com o que
-        aprendemos até o momento, em que pude fazer a comparação tanto do campo contato quanto do campo número com uma condição simples.*/
-
-    } else{
-
-        contato[inputnomeContato.value]=(inputnumeroContato.value); 
-        numero.push(parseFloat(inputnumeroContato.value));
-
-        let linha ='<tr>';
-        linha += `<td>${inputnomeContato.value}</td>`
-        linha += `<td>${inputnumeroContato.value}</td>`
-        linha += '</tr>';
-
-        linhas += linha;
-    }
-
-    inputnomeContato.value='';
-    inputnumeroContato.value='';
-}
-
-function atualizaTabela(){ 
-    const corpoTabela=document.querySelector('tbody');
-    corpoTabela.innerHTML=linhas;
-}
