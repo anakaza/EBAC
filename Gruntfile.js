fca04dd386c7db709c3551d3462dae87c99f3aeb
azk <linarelolcs@gmail.com>
2024-02-27 10:07:26 -0300

    Initial commit

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,123 +1,38 @@
-module.exports = function(grunt){
+// 1) Criar um arquivo Gruntfile;
+
+module.exports = function (grunt){
+
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-
-        less:{
+        less: {
             development:{
-                files:{
-                    'dev/styles/main.css':'src/styles/main.less'
-                }
-            },
-            production:{
                 options:{
-                    compress:true,
+                    paths: ['src/less'],
                 },
+// 2) Executar a compilação do LESS sem a minificação;
                 files:{
-                    'dist/styles/main.min.css':'src/styles/main.less'
+                    'dist/styles/main.css':'src/*.less' 
                 }
             }
         },
 
-        watch:{
-            less: {
-                files:['src/styles/**/*.less'],
-                tasks: ['less:development']
+        uglify:{
+            options:{
+// 3) Executar a compressão de código JavaScript;
+                compress:true
             },
-
-            html:{
-                files:['src/index.html'],
-                tasks: ['replace:dev']
-            }
-        },
-
-        //Substituição
-        replace: {
-            dev: {
-                options: {
-                    patterns:[
-                        {
-                            match: 'ENDERECO_DO_CSS',
-                            replacement: './styles/main.css'
-                        },
-                        {
-                            match: 'ENDERECO_DO_JS',
-                            replacement: '../src/scripts/main.js'
-                        }
-                    ]
-                },
-
-                files: [
-                    {
-                        expand: true,
-                        flatten: true,
-                        src: ['src/index.html'],
-                        dest: 'dev/'
-                    }
-                ]
-            },
-
-            //substituição
-            dist:{
-                options: {
-                    patterns:[
-                        {
-                        match: 'ENDERECO_DO_CSS',
-                        replacement: './styles/main.min.css'
-                        },
-                        {
-                            match: 'ENDERECO_DO_JS',
-                            replacement: './scripts/main.min.js'
-                        }
-                        ]
-                        },
-
-                files:  [
-                        {
-                        expand: true,
-                        flatten: true,
-                        src: ['prebuild/index.html'],
-                        dest: 'dist/'
-                        }
-                        ]
-                }
-        },
-        //minificação HTML
-        htmlmin: {
-            dist: {
-
-                options:{
-                    removeComments: true,
-                    collapseWhitespace: true
-                },
-
-                files:{
-                    'prebuild/index.html':'src/index.html'
-                }
-            }
-        },
-
-        //limpeza
-        clean:['prebuild'],
-        uglify: {
-            target:{
-                files:{
-                    'dist/scripts/main.min.js':'src/scripts/main.js'
-                }
+        compressao:{
+            files:{
+                'dist/scripts/main.min.js': ['src/*.js']
             }
         }
-    })
+
+    }
+
+    });
 
     grunt.loadNpmTasks('grunt-contrib-less');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-replace')
-    grunt.loadNpmTasks('grunt-contrib-htmlmin');
-    grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-uglify');
 
-    grunt.registerTask('default', ['watch']);
-    grunt.registerTask('build', ['less:production','htmlmin:dist', 'replace:dist', 'clean', 'uglify']);
-    
+    grunt.registerTask('default',['less','uglify']);
 
 }
-
-
---
+++ src/main.js
@@ -0,0 +1,1 @@
+//Compilação JS tarefa
